c72af85ba27ba39bc5bf8ebe07e713cd099f778a
OlesyaGuyman <olesya.guyman@yandex.ru>
2022-12-09 21:04:54 +0300

    fix

---
+++ 24/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36 */
+
+//метод, который получает число с консоли
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод. Введите число больше 0.");
+        }
+    }
+
+    return result;
+}
+
+//метод, который считает сумму от 1 до n
+int GetSumm(int number)
+{
+    int summ = 0;
+
+    for (int i = 1; i <= number; i++)
+    {
+        summ += i;
+    }
+
+    return summ;
+}
+
+int number = GetNumber("Введите число больше 0:");
+int summ = GetSumm(number);
+
+Console.WriteLine($"сумма чисел от 1 до {number} = {summ}");
---
+++ 30/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0] */
+
+int [] number = new int[8];
+Random rnd = new Random();
+
+for (int i = 0; i < number.Length; i++)
+{
+    number[i] = rnd.Next(0, 2);
+    Console.Write(number[i]+" ");
+}
+
+/* int[] massive = new int[8];
+Random rnd = new Random();
+for (int i = 0; i < massive.Length; i++)
+{
+    massive[i] = rnd.Next(0, 2);
+    System.Console.Write(massive[i]);
+} */
